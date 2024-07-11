74dedc7c9e4b5f40dd4870c3db7f590e18a302ef
Ishaq Breiwish <61039667+ishaqbreiwish@users.noreply.github.com>
2024-07-12 01:27:33 +0400

    Update views.py

--- page_generator/landing_page/views.py
+++ page_generator/landing_page/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MyModelForm
 
 # Create your views here.
+def my_view(request):
+    if request.method == 'POST':
+        form = MyModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = MyModelForm
+    return render(request, 'landing_page/form.html', {'form': form})
+
+def success_view(request):
+    return render(request, 'landing_page/success.html')
